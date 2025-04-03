740f81ec5e3ff2c4493a253a28078c3a040aefc2
Jennifer Li <50808350+jennifer-hy-li@users.noreply.github.com>
2025-04-03 15:00:31 -0400

    added todo

--- brainy-bytes/src/App.js
+++ brainy-bytes/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      //TODO: add custom homepage, remove boilerplate code
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+            
     </div>
   );
 }
