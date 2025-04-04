c77bf1f2226096bd1a104b5a63705bf16c9ca09d
Jennifer Li <jennifer.lihuiyan@gmail.com>
2025-04-04 00:03:36 -0400

    Fixed routing mechanism on App.js

--- brainy-bytes/src/App.js
+++ brainy-bytes/src/App.js
@@ -1,26 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Levels from './pages/Levels';
+
 
 function App() {
   return (
-    <div className="App">
-      //TODO: add custom homepage, remove boilerplate code
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-            
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Levels" element={<Levels />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
