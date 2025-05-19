d5c4186d306f07a07fb0a4f0ee0d8769941a8b63
Jennifer Li <jennifer.lihuiyan@gmail.com>
2025-05-19 19:14:05 -0400

    Added aws dependencies and Login page

---
+++ brainy-bytes/src/pages/AuthPage.js
@@ -0,0 +1,16 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+function AuthPage() {
+    return (
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user?.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
+    )
+}
+
+export default AuthPage;
